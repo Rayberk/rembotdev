5ffebeb022e84422809709766a18c903e3dbd02e
Remzi Ayberk Çimen <68689359+Rayberk@users.noreply.github.com>
2020-08-22 14:01:59 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -10,7 +10,7 @@ module.exports ={
         
         const banlayan = message.member;
         const perm = message.member.hasPermission(0x00000004);
-        const botperm = message.guild.me.has(0x00000004);
+        const botperm = message.client.has(0x00000004);
         let reason = args.slice(1).join(" ");
 
         const embed = new Discord.MessageEmbed()
