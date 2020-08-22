935da809279e947bf6df0f094906d44e21cb89e1
Remzi Ayberk Çimen <68689359+Rayberk@users.noreply.github.com>
2020-08-22 13:41:13 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -8,8 +8,8 @@ module.exports ={
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         
         const banlayan = message.GuildMember;
-        const perm = message.GuildMember.hasPermission('KICK_MEMBERS');
-        const botperm = message.guild.me.has('KICK_MEMBERS');
+        const perm = message.GuildMember.hasPermission(0x00000002);
+        const botperm = message.guild.me.has(0x00000002);
         let reason = args.slice(1).join(" ");
 
         const embed = new Discord.MessageEmbed()
