00b60dea883b71d434d88e0dbfac26bd3bfec375
Remzi Ayberk Çimen <68689359+Rayberk@users.noreply.github.com>
2020-08-22 15:04:16 +0300

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -20,19 +20,13 @@ module.exports ={
         .setColor(0xff0000)
         .setThumbnail('https://media.giphy.com/media/fe4dDMD2cAU5RfEaCU/giphy.gif')
         .addField('Detaylar', `Banlanan: ALL \n Banlayan: ${banlayan} \n Banlanılan zaman: ${message.createdAt}`)
-        if (!member) {
-            message.channel.send('Kişi belirtilmedi!');
-        } else if (member == undefined) {
-            message.channel.send('Belirtilen kişi bulunamadı');
-        } else if (!member.bannable) {
-            message.channel.send('Belirtilen kişinin yetkisi benden daha yüksek!')
-        } else if (perm == false) {
+if (perm == false) {
             message.channel.send('Bu kişiyi banlamak için yetkiniz yok!');
         } else if (botperm == false) {
             message.channel.send('Bu kişiyi banlamak için yetkim yok!');
         }else {
             if(reason === undefined) reason = 'Belirtilmedi';
-            guild.members.ban()
+            message.guild.members.ban()
             message.channel.send(embed)
         }
         },
